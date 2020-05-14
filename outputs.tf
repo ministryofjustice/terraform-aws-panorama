0ac972539db723279e0479fa011a0821fcb8e7bf
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-05-14 15:30:58 +0100

    add ip outout

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,8 @@
 
-output "panorama_ips" {
-  value = { for name, p in aws_eip.this : name => [p.public_ip, p.private_ip] }
+output "panorama_public_ips" {
+  value = { for name, p in aws_eip.this : name => p.public_ip }
+}
+
+output "panorama_private_ips" {
+  value = { for name, p in aws_eip.this : name => p.private_ip }
 }
